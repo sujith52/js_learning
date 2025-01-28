b95ca707b39409e5ac4daca11bb62eb8959db8fd
sujith kumar <148097955+sujith52@users.noreply.github.com>
2025-01-28 20:48:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+// let isJsProgrammingLanguage = true;
+// let isJsHard = false;
+// let favNum = 4;
+// console.log(isJsProgrammingLanguage);
+// console.log(isJsHard);
+// console.log(favNum + undefined);
+
+// let firstFavNumb = 6;
+// let secondFavNumb = 5;
+
+// console.log(firstFavNumb > secondFavNumb);
+// console.log(firstFavNumb < secondFavNumb);
+// console.log(firstFavNumb >= secondFavNumb);
+// console.log(firstFavNumb <= secondFavNumb);
+// console.log(firstFavNumb === secondFavNumb);
+// console.log(firstFavNumb !== secondFavNumb);
+// console.log(firstFavNumb == secondFavNumb);
+// console.log(firstFavNumb !== secondFavNumb);
+
+// let favActorFirstName = "jennifer";
+// let favActorLastName = "lawerance";
+// let fullName = favActorFirstName + favActorLastName;
+// let uppercase = fullName.toUpperCase();
+// let message = "My fav actor is " + uppercase + " is very good";
+// console.log(message + " his best show in silicon valley");
+// console.log(fullName);
+// console.log(uppercase);
+// console.log(message);
+
+let fiv = "jhon";
+let las = "wick";
+let full = fiv + las;
+let mess = `hello actor ${full}`;
+console.log(mess);
